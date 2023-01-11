241c58dbcb3b9e09171e38977630b4163c30cc3b
futy18 <fuvty@outlook.com>
2023-01-11 13:55:16 +0000

    [update] add spmm_mean and comments

--- dgsparse/spmm.py
+++ dgsparse/spmm.py
@@ -5,9 +5,36 @@ from dgsparse.tensor import SparseTensor
 
 # torch.ops.dgsparse.SpMM
 
+
 def spmm_sum(sparse: SparseTensor, dense: torch.Tensor) -> torch.Tensor:
+    r"""
+    Matrix multiplication of a sparse tensor and a dense tensor with sum reduction.
+
+    Args:
+        sparse (SparseTensor): The sparse tensor.
+        dense (Tensor): The dense tensor.
+
+    rtype: :class:'Tensor'
+    """
     has_value = sparse.has_value
     rowptr = sparse.rowptr
     col = sparse.col
     values = sparse.values
     return torch.ops.dgsparse.spmm_sum(rowptr, col, values, dense, has_value)
+
+
+def spmm_mean(sparse: SparseTensor, dense: torch.Tensor) -> torch.Tensor:
+    r"""
+    Matrix multiplication of a sparse tensor and a dense tensor with mean reduction.
+
+    Args:
+        sparse (SparseTensor): The sparse tensor.
+        dense (Tensor): The dense tensor.
+
+    rtype: :class:'Tensor'
+    """
+    has_value = sparse.has_value
+    rowptr = sparse.rowptr
+    col = sparse.col
+    values = sparse.values
+    return torch.ops.dgsparse.spmm_mean(rowptr, col, values, dense, has_value)
