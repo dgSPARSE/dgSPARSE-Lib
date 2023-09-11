9cbdb7f122bcd7a801e932d35f09830a9e594430
fishming <1661342068@qq.com>
2023-09-11 00:40:39 -0700

    [Update] SpMM Test

--- test/test_spmm.py
+++ test/test_spmm.py
@@ -5,54 +5,27 @@ from scipy.io import mmread
 from dgsparse import spmm_sum, spmm_max, spmm_min, spmm_mean
 from dgsparse import SparseTensor
 import torch_sparse
-
+import pytest
 
 class SpMMSum:
-    def __init__(self, path, in_dim, device, algorithm) -> None:
-        sparsecsr = mmread(path).astype("float32").tocsr()
-        rowptr = torch.from_numpy(sparsecsr.indptr).to(device).int()
-        colind = torch.from_numpy(sparsecsr.indices).to(device).int()
-        weight = torch.from_numpy(sparsecsr.data).to(device).float()
-        self.rowptr = rowptr
-        self.colind = colind
-        sparsecoo = sparsecsr.tocoo()
-
-        # prepare for pytorch_sparse
-        row = torch.from_numpy(sparsecoo.row).type(torch.int64)
-        col = torch.from_numpy(sparsecoo.col).type(torch.int64)
-        print(rowptr)
-        print(colind)
-        # print(weight)
-        index = torch.cat((row.unsqueeze(0), col.unsqueeze(0)), 0)
-        self.index = index.to(device)
-        self.m = sparsecoo.shape[0]
-        self.n = sparsecoo.shape[1]
-        self.weight = weight.requires_grad_()
-
+    def __init__(self, data, in_dim, device, algorithm) -> None:
         # prepare for torch and dgsparse
-        self.tcsr = torch.sparse_csr_tensor(
-            rowptr, colind, weight, dtype=torch.float, requires_grad=True
-        )
+        self.tcsr = data.tcsr
 
         self.dcsr = SparseTensor.from_torch_sparse_csr_tensor(
             self.tcsr.clone().detach(), True, requires_grad=True
         )
-        nodes = rowptr.size(0) - 1
-        self.nodes = nodes
+
         self.in_dim = in_dim
         self.device = device
         self.algorithm = algorithm
         self.input_feature = torch.rand(
-            (nodes, in_dim), requires_grad=True, device=device
+            (data.num_nodes, in_dim), requires_grad=True, device=device
         )
-        # don't use copy to device, or may can not implement backward_check
 
     def forward_check(self):
         out_check = torch.sparse.mm(self.tcsr, self.input_feature)
         out = spmm_sum(self.dcsr, self.input_feature, self.algorithm)
-        # print(out-out_check)
-        print(out)
-        print(out_check)
         assert torch.allclose(out, out_check) == True
 
     def backward_check(self):
@@ -61,204 +34,105 @@ class SpMMSum:
         dX_check = self.input_feature.grad
         dA_check = self.tcsr.grad
         out = spmm_sum(self.dcsr, self.input_feature, self.algorithm)
-        out.retain_grad()
         out.sum().backward()
         dX = self.input_feature.grad
         dA_nnz = self.dcsr.storage._values.grad
 
-        pyg_check = torch_sparse.spmm(
-            self.index, self.weight, self.m, self.n, self.input_feature
+        assert torch.allclose(dX, dX_check) == True
+        assert torch.allclose(dA_nnz, dA_check.values()) == True
+
+class SpMMMax:
+    def __init__(self, data, in_dim, device, algorithm) -> None:
+        # prepare for torch and dgsparse
+        self.tcsr = data.tcsr
+
+        self.dcsr = SparseTensor.from_torch_sparse_csr_tensor(
+            self.tcsr.clone().detach(), True, requires_grad=True
         )
-        pyg_check.sum().backward()
+
+        self.in_dim = in_dim
+        self.device = device
+        self.algorithm = algorithm
+        self.input_feature = torch.rand(
+            (data.num_nodes, in_dim), requires_grad=True, device=device
+        )
+
+    def forward_check(self):
+        out_check = torch.sparse.mm(self.tcsr.cpu(), self.input_feature.cpu(), "max").cuda()
+        out = spmm_max(self.dcsr, self.input_feature, self.algorithm)
+        assert torch.allclose(out, out_check) == True
+
+    def backward_check(self):
+        out_check = torch.sparse.mm(self.tcsr.cpu(), self.input_feature.cpu(), "max").cuda()
+        out_check.sum().backward()
+        dX_check = self.input_feature.grad
+        dA_check = self.tcsr.grad
+        out = spmm_max(self.dcsr, self.input_feature, self.algorithm)
+        out.sum().backward()
+        dX = self.input_feature.grad
+        dA_nnz = self.dcsr.storage._values.grad
+
+        assert torch.allclose(dX, dX_check) == True
+        assert torch.allclose(dA_nnz, dA_check.values()) == True
+
+class SpMMMin:
+    def __init__(self, data, in_dim, device, algorithm) -> None:
+        # prepare for torch and dgsparse
+        self.tcsr = data.tcsr
+
+        self.dcsr = SparseTensor.from_torch_sparse_csr_tensor(
+            self.tcsr.clone().detach(), True, requires_grad=True
+        )
+
+        self.in_dim = in_dim
+        self.device = device
+        self.algorithm = algorithm
+        self.input_feature = torch.rand(
+            (data.num_nodes, in_dim), requires_grad=True, device=device
+        )
+
+    def forward_check(self):
+        out_check = torch.sparse.mm(self.tcsr.cpu(), self.input_feature.cpu(), "min").cuda()
+        out = spmm_min(self.dcsr, self.input_feature, self.algorithm)
+        assert torch.allclose(out, out_check) == True
+
+    def backward_check(self):
+        out_check = torch.sparse.mm(self.tcsr.cpu(), self.input_feature.cpu(), "min").cuda()
+        out_check.sum().backward()
+        dX_check = self.input_feature.grad
+        dA_check = self.tcsr.grad
+        out = spmm_min(self.dcsr, self.input_feature, self.algorithm)
+        out.sum().backward()
+        dX = self.input_feature.grad
+        dA_nnz = self.dcsr.storage._values.grad
 
         assert torch.allclose(dX, dX_check) == True
         assert torch.allclose(dA_nnz, dA_check.values()) == True
 
 
-class SpMMMax:
-    def __init__(self, path, in_dim, device, algorithm) -> None:
-        sparsecsr = mmread(path).astype("float32").tocsr()
-        rowptr = torch.from_numpy(sparsecsr.indptr).to(device).int()
-        colind = torch.from_numpy(sparsecsr.indices).to(device).int()
-        weight = torch.from_numpy(sparsecsr.data).to(device).float()
-        self.rowptr = rowptr
-        self.colind = colind
-        sparsecoo = sparsecsr.tocoo()
-
-        # prepare for pytorch_sparse
-        row = torch.from_numpy(sparsecoo.row).type(torch.int64)
-        col = torch.from_numpy(sparsecoo.col).type(torch.int64)
-        print(rowptr)
-        print(colind)
-        # print(weight)
-        index = torch.cat((row.unsqueeze(0), col.unsqueeze(0)), 0)
-        self.index = index.to(device)
-        self.m = sparsecoo.shape[0]
-        self.n = sparsecoo.shape[1]
-        self.weight = weight
-
-        # prepare for torch and dgsparse
-        self.tcsr = torch.sparse_csr_tensor(
-            rowptr, colind, weight, dtype=torch.float, requires_grad=True
-        )
-        self.dcsr = SparseTensor.from_torch_sparse_csr_tensor(
-            self.tcsr.detach(), True, requires_grad=True
-        )
-        nodes = rowptr.size(0) - 1
-        self.nodes = nodes
-        self.in_dim = in_dim
-        self.device = device
-        self.algorithm = algorithm
-        self.input_feature = torch.rand(
-            (nodes, in_dim), requires_grad=True, device=device
-        )
-        # don't use copy to device, or may can not implement backward_check
-
-    def forward_check(self):
-        out_check = torch.sparse.mm(
-            self.tcsr.cpu(), self.input_feature.cpu(), "max"
-        ).cuda()
-        out = spmm_max(self.dcsr, self.input_feature, self.algorithm)
-        print(out - out_check)
-        assert torch.allclose(out, out_check) == True
-
-    def backward_check(self):
-        out_check = torch.sparse.mm(
-            self.tcsr.cpu(), self.input_feature.cpu(), "max"
-        ).cuda()
-        out_check.sum().backward()
-        dX_check = self.input_feature.grad
-        dA_check = self.tcsr.grad
-        out = spmm_max(self.dcsr, self.input_feature, self.algorithm)
-        out.sum().backward()
-        dX = self.input_feature.grad
-        dA_nnz = self.dcsr.storage._values.grad
-        dA = torch.sparse_csr_tensor(
-            self.rowptr, self.colind, dA_nnz, dtype=torch.float
-        )
-        print(dA)
-        print(dA_check)
-        print(dA.values() - dA_check.values())
-        assert torch.allclose(dX, dX_check) == True
-        assert torch.allclose(dA.values(), dA_check.values()) == True
-
-
-class SpMMMin:
-    def __init__(self, path, in_dim, device, algorithm) -> None:
-        sparsecsr = mmread(path).astype("float32").tocsr()
-        rowptr = torch.from_numpy(sparsecsr.indptr).to(device).int()
-        colind = torch.from_numpy(sparsecsr.indices).to(device).int()
-        weight = torch.from_numpy(sparsecsr.data).to(device).float()
-        self.rowptr = rowptr
-        self.colind = colind
-        sparsecoo = sparsecsr.tocoo()
-
-        # prepare for pytorch_sparse
-        row = torch.from_numpy(sparsecoo.row).type(torch.int64)
-        col = torch.from_numpy(sparsecoo.col).type(torch.int64)
-        print(rowptr)
-        print(colind)
-        # print(weight)
-        index = torch.cat((row.unsqueeze(0), col.unsqueeze(0)), 0)
-        self.index = index.to(device)
-        self.m = sparsecoo.shape[0]
-        self.n = sparsecoo.shape[1]
-        self.weight = weight
-
-        # prepare for torch and dgsparse
-        self.tcsr = torch.sparse_csr_tensor(
-            rowptr, colind, weight, dtype=torch.float, requires_grad=True
-        )
-        self.dcsr = SparseTensor.from_torch_sparse_csr_tensor(
-            self.tcsr.detach(), True, requires_grad=True
-        )
-        nodes = rowptr.size(0) - 1
-        self.nodes = nodes
-        self.in_dim = in_dim
-        self.device = device
-        self.algorithm = algorithm
-        self.input_feature = torch.rand(
-            (nodes, in_dim), requires_grad=True, device=device
-        )
-        # don't use copy to device, or may can not implement backward_check
-
-    def forward_check(self):
-        out_check = torch.sparse.mm(
-            self.tcsr.cpu(), self.input_feature.cpu(), "min"
-        ).cuda()
-        out = spmm_min(self.dcsr, self.input_feature, self.algorithm)
-        print(out - out_check)
-        assert torch.allclose(out, out_check) == True
-
-    def backward_check(self):
-        out_check = torch.sparse.mm(
-            self.tcsr.cpu(), self.input_feature.cpu(), "min"
-        ).cuda()
-        out_check.sum().backward()
-        dX_check = self.input_feature.grad
-        dA_check = self.tcsr.grad
-        out = spmm_min(self.dcsr, self.input_feature, self.algorithm)
-        out.sum().backward()
-        dX = self.input_feature.grad
-        dA_nnz = self.dcsr.storage._values.grad
-        dA = torch.sparse_csr_tensor(
-            self.rowptr, self.colind, dA_nnz, dtype=torch.float
-        )
-        assert torch.allclose(dX, dX_check) == True
-        assert torch.allclose(dA.values(), dA_check.values()) == True
-
-
 class SpMMMean:
-    def __init__(self, path, in_dim, device, algorithm) -> None:
-        sparsecsr = mmread(path).astype("float32").tocsr()
-        rowptr = torch.from_numpy(sparsecsr.indptr).to(device).int()
-        colind = torch.from_numpy(sparsecsr.indices).to(device).int()
-        weight = torch.from_numpy(sparsecsr.data).to(device).float()
-        self.rowptr = rowptr
-        self.colind = colind
-        sparsecoo = sparsecsr.tocoo()
-
-        # prepare for pytorch_sparse
-        row = torch.from_numpy(sparsecoo.row).type(torch.int64)
-        col = torch.from_numpy(sparsecoo.col).type(torch.int64)
-        print(rowptr)
-        print(colind)
-        # print(weight)
-        index = torch.cat((row.unsqueeze(0), col.unsqueeze(0)), 0)
-        self.index = index.to(device)
-        self.m = sparsecoo.shape[0]
-        self.n = sparsecoo.shape[1]
-        self.weight = weight
-
+    def __init__(self, data, in_dim, device, algorithm) -> None:
         # prepare for torch and dgsparse
-        self.tcsr = torch.sparse_csr_tensor(
-            rowptr, colind, weight, dtype=torch.float, requires_grad=True
-        )
+        self.tcsr = data.tcsr
+
         self.dcsr = SparseTensor.from_torch_sparse_csr_tensor(
-            self.tcsr.detach(), True, requires_grad=True
+            self.tcsr.clone().detach(), True, requires_grad=True
         )
-        nodes = rowptr.size(0) - 1
-        self.nodes = nodes
+
         self.in_dim = in_dim
         self.device = device
         self.algorithm = algorithm
         self.input_feature = torch.rand(
-            (nodes, in_dim), requires_grad=True, device=device
+            (data.num_nodes, in_dim), requires_grad=True, device=device
         )
-        # don't use copy to device, or may can not implement backward_check
 
     def forward_check(self):
-        out_check = torch.sparse.mm(
-            self.tcsr.cpu(), self.input_feature.cpu(), "mean"
-        ).cuda()
+        out_check = torch.sparse.mm(self.tcsr.cpu(), self.input_feature.cpu(), "mean").cuda()
         out = spmm_mean(self.dcsr, self.input_feature, self.algorithm)
-        print(out - out_check)
         assert torch.allclose(out, out_check) == True
 
     def backward_check(self):
-        out_check = torch.sparse.mm(
-            self.tcsr.cpu(), self.input_feature.cpu(), "mean"
-        ).cuda()
+        out_check = torch.sparse.mm(self.tcsr.cpu(), self.input_feature.cpu(), "mean").cuda()
         out_check.sum().backward()
         dX_check = self.input_feature.grad
         dA_check = self.tcsr.grad
@@ -266,20 +140,51 @@ class SpMMMean:
         out.sum().backward()
         dX = self.input_feature.grad
         dA_nnz = self.dcsr.storage._values.grad
-        dA = torch.sparse_csr_tensor(
-            self.rowptr, self.colind, dA_nnz, dtype=torch.float
-        )
-        print(dA)
-        print(dA_check)
-        # print(dX)
-        # print(dX_check)
+
         assert torch.allclose(dX, dX_check) == True
-        assert torch.allclose(dA.values(), dA_check.values()) == True
+        assert torch.allclose(dA_nnz, dA_check.values()) == True
 
 
-def test_spmm():
-    op_list = [SpMMMax, SpMMMean, SpMMSum, SpMMMin]
-    for op in op_list:
-        gc = op("../example/data/p2p-Gnutella31.mtx", 32, 0, 0)
-        gc.forward_check()
-        gc.backward_check()
+from utils import GraphDataset
+
+datasets = ["cora", "citeseer", "pubmed", "ppi"]
+features = [32, 64, 128]
+@pytest.mark.parametrize('dataset', datasets)
+@pytest.mark.parametrize('feat', features)
+def test_spmm_sum(dataset, feat):
+    data = GraphDataset(dataset, 0)
+    gc = SpMMSum(data, feat, 0, 0)
+    gc.forward_check()
+    gc.backward_check()
+
+
+datasets = ["cora", "citeseer", "pubmed", "ppi"]
+features = [32, 64, 128]
+@pytest.mark.parametrize('dataset', datasets)
+@pytest.mark.parametrize('feat', features)
+def test_spmm_max(dataset, feat):
+    data = GraphDataset(dataset, 0)
+    gc = SpMMMax(data, feat, 0, 0)
+    gc.forward_check()
+    gc.backward_check()
+
+datasets = ["cora", "citeseer", "pubmed", "ppi"]
+features = [32, 64, 128]
+@pytest.mark.parametrize('dataset', datasets)
+@pytest.mark.parametrize('feat', features)
+def test_spmm_min(dataset, feat):
+    data = GraphDataset(dataset, 0)
+    gc = SpMMMin(data, feat, 0, 0)
+    gc.forward_check()
+    gc.backward_check()
+
+datasets = ["cora", "citeseer", "pubmed", "ppi"]
+features = [32, 64, 128]
+@pytest.mark.parametrize('dataset', datasets)
+@pytest.mark.parametrize('feat', features)
+def test_spmm_mean(dataset, feat):
+    data = GraphDataset(dataset, 0)
+    gc = SpMMMean(data, feat, 0, 0)
+    gc.forward_check()
+    gc.backward_check()
+
---
+++ test/utils.py
@@ -0,0 +1,53 @@
+import torch_geometric.datasets as datasets
+from ogb.nodeproppred import PygNodePropPredDataset
+from torch_geometric.utils import to_scipy_sparse_matrix
+import torch
+
+
+class GraphDataset():
+    def __init__(self, name: str, device):
+        self.name = name
+        self.device = device
+        self.get_dataset()
+
+    def get_dataset(self):
+        if self.name == "arxiv":
+            arxiv = PygNodePropPredDataset(root="./data/", name="ogbn-arxiv")
+            graph = arxiv[0]
+        elif self.name == "proteins":
+            proteins = PygNodePropPredDataset(root="./data/", name="ogbn-proteins")
+            graph = proteins[0]
+        elif self.name == "products":
+            products = PygNodePropPredDataset(root="./data/", name="ogbn-products")
+            graph = products[0]
+        elif self.name == "pubmed":
+            dataset = datasets.Planetoid(root="./data/", name="Pubmed")
+            graph = dataset[0]
+        elif self.name == "citeseer":
+            dataset = datasets.Planetoid(root="./data/", name="Citeseer")
+            graph = dataset[0]
+        elif self.name == "cora":
+            dataset = datasets.Planetoid(root="./data/", name="Cora")
+            graph = dataset[0]
+        elif self.name == "ppi":
+            dataset = datasets.PPI(root="./data/")
+            graph = dataset[0]
+        elif self.name == "reddit":
+            dataset = datasets.Reddit(root="./data/Reddit")
+            graph = dataset[0]
+        elif self.name == "github":
+            dataset = datasets.GitHub(root="./data/")
+            graph = dataset[0]
+        else:
+            raise KeyError("Unknown dataset {}.".format(self.name))
+        scipy_coo = to_scipy_sparse_matrix(graph.edge_index, num_nodes=graph.num_nodes)
+        scipy_csr = scipy_coo.tocsr()
+        rowptr = scipy_csr.indptr
+        col = scipy_csr.indices
+        weight = torch.ones(col.shape, requires_grad=True)
+        self.num_nodes = graph.num_nodes
+        self.tcsr = torch.sparse_csr_tensor(
+            rowptr, col, weight, dtype=torch.float, size=(self.num_nodes, self.num_nodes),  requires_grad=True, device=self.device
+        )
+        
+
